7c374192c5469e1ffae4dd646d874f6e1849bc5b
Asmaalafe55 <asmaalafe55@gmail.com>
2022-12-14 20:50:35 +0200

    first commit

---
+++ pages/events/event1.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const event1 = () => {
+  return <div>event1</div>;
+};
+
+export default event1;
---
+++ pages/events/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const index = () => {
+  return (
+    <div>
+      <h1>Events page</h1>
+      <div>
+        <a href="">
+          <img />
+          <h2>Events in London</h2>
+        </a>
+        <a href="">
+          <img />
+          <h2>Events in Francisco</h2>
+        </a>
+        <a href="">
+          <img />
+          <h2>Events in Barcelona</h2>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default index;
