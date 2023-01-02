890119735ea88928a6354a6b7726d91ae9497c08
Asmaalafe55 <asmaalafe55@gmail.com>
2023-01-02 14:54:40 +0200

    editing home page

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -13,26 +13,27 @@ const HomePage = ({ data }) => {
   return (
     <main className={styles.home}>
       <Header id="home" subEmail={subEmail} setSubEmail={setSubEmail} />
-
-      <div className={styles.app__home}>
-        <div className={styles.title}>Top Events</div>
-        {data.map((event) => (
-          <Link key={event.id} href={`/events/${event.id}`}>
-            <Image
-              width={200}
-              height={200}
-              alt={event.title}
-              src={event.image}
-            />
-            <div>
-              <h2>{event.title}</h2>
-              <p>{event.description}</p>
-            </div>
-          </Link>
-        ))}
-        <div className={styles.contact__title}>Contact Us</div>
+      <div className={styles.container}>
+        <div className={styles.app__home}>
+          <div className={styles.title}>Top Events</div>
+          {data.map((event) => (
+            <Link key={event.id} href={`/events/${event.id}`}>
+              <Image
+                width={200}
+                height={200}
+                alt={event.title}
+                src={event.image}
+              />
+              <div>
+                <h2>{event.title}</h2>
+                <p>{event.description}</p>
+              </div>
+            </Link>
+          ))}
+          <div className={styles.contact__title}>Contact Us</div>
+        </div>
+        <div className={styles.app__home2}></div>
       </div>
-
       <Contact />
     </main>
   );
