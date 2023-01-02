c1b93a136ceed23a3d65c391445942bdb131b83e
Asmaalafe55 <asmaalafe55@gmail.com>
2023-01-02 16:25:19 +0200

    adding style to events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,27 +1,10 @@
 import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+
+import EventsPage from '../../src/components/events/EventsPage';
 
 const Events = (props) => {
   const data = props.data;
-  return (
-    <div>
-      <h1>Events Page</h1>
-      <div>
-        {data.map((event) => (
-          <Link key={event.id} href={`/events/${event.id}`}>
-            <Image
-              width={200}
-              height={200}
-              alt={event.title}
-              src={event.image}
-            />
-            <h2>{event.title}</h2>
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
+  return <EventsPage data={data} />;
 };
 
 export default Events;
---
+++ src/components/events/eventsPage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import styles from './EventsPage.module.scss';
+
+const eventsPage = ({ data }) => {
+  return (
+    <div className={styles.events}>
+      <label>Events Page</label>
+      <div>
+        {data.map((event) => (
+          <Link key={event.id} href={`/events/${event.id}`}>
+            <Image
+              width={200}
+              height={200}
+              alt={event.title}
+              src={event.image}
+            />
+            <h2>{event.title}</h2>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default eventsPage;
