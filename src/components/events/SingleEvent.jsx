739018c49daf81d9d948428a12605a17f2d6be58
Asmaalafe55 <asmaalafe55@gmail.com>
2023-01-03 01:36:49 +0200

    adding style to single event 🥱

--- src/components/events/SingleEvent.jsx
+++ src/components/events/SingleEvent.jsx
@@ -5,21 +5,30 @@ import styles from './SingleEvent.module.scss';
 
 const SingleEvent = ({ data, func, message, inputEmail }) => {
   return (
-    <div>
+    <div className={styles.single_event_container}>
+      <div className={styles.single_event}>
+        <div>
+          <h1>{data.title}</h1>
+          <p>{data.description}</p>
+        </div>
+
+        <div>
+          <div>
+            <label>Get Registered for this event!</label>
+            <form onSubmit={func}>
+              <input
+                ref={inputEmail}
+                type="email"
+                id="email"
+                placeholder="Please insert your email here .."
+              />
+              <button type="submit">Submit</button>
+            </form>
+            <p>{message}</p>
+          </div>
+        </div>
+      </div>
       <Image src={data.image} width={1000} height={500} alt={data.title} />
-      <h1>{data.title}</h1>
-      <p>{data.description}</p>
-      <label>Get Registered for this event!</label>
-      <form onSubmit={func}>
-        <input
-          ref={inputEmail}
-          type="email"
-          id="email"
-          placeholder="Please insert your email here .."
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <p>{message}</p>
     </div>
   );
 };
