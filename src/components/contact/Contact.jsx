f198cc5bb71a89eb2e802b5fe7e52af023560c72
Asmaalafe55 <asmaalafe55@gmail.com>
2023-01-27 00:14:14 +0200

    adding post request to the contact us forum

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,35 +1,68 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './Contact.module.scss';
+import axios from '../../../utils/axios';
 
 import { HiPhone, HiMail } from 'react-icons/hi';
 
-const Contact = () => {
+const Contact = (props) => {
+  const inputEmail = useRef();
+  const inputName = useRef();
+  const inputMessage = useRef();
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      name: inputName.current.value,
+      email: inputEmail.current.value,
+      message: inputMessage.current.value,
+    };
+
+    try {
+      const response = await axios.post('/contact', data);
+
+      inputName.current.value = '';
+      inputEmail.current.value = '';
+      inputMessage.current.value = '';
+      setMessage(response.data.message);
+      alert('Your message has been sent!');
+    } catch (e) {
+      console.log('ERROR', e);
+    }
+  };
+
   return (
     <>
       <div id="contact" className={styles.app__contact}>
-        {/* <div className={styles.left__right}> */}
         <div className={styles.contact_left_side}>
-          <div>
-            <div>
-              <label>Full Name:</label>
-              <input type="name" placeholder="Please enter your full name" />
-            </div>
-            <div>
-              <label>Email:</label>
-              <input type="email" placeholder="Please enter your email" />
-            </div>
-          </div>
-          <div>
-            <div>
-              <label>Message:</label>
-              <input
-                className={styles.custom_input}
-                type="message"
-                placeholder="Please enter your message"
-              />
-            </div>
-            <button>send</button>
-          </div>
+          <form onSubmit={handleSubmit}>
+            <label>Full Name:</label>
+            <input
+              ref={inputName}
+              type="text"
+              id="name"
+              placeholder="Please enter your full name"
+            />
+
+            <label>Email:</label>
+            <input
+              ref={inputEmail}
+              type="email"
+              id="email"
+              placeholder="Please enter your email"
+            />
+
+            <label>Message:</label>
+            <input
+              className={styles.custom_input}
+              ref={inputMessage}
+              type="text"
+              id="message"
+              placeholder="Please enter your message"
+            />
+            <button type="submit">Subscribe</button>
+          </form>
         </div>
         <div className={styles.contact_right_side}>
           <div>
@@ -42,7 +75,6 @@ const Contact = () => {
           </div>
         </div>
       </div>
-      {/* </div> */}
     </>
   );
 };
