0b2fe8cf3c5bdd44cb4e78f874085e48fa1d1984
Asmaalafe55 <asmaalafe55@gmail.com>
2023-02-06 23:15:08 +0200

    changing the trendings cards style

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -8,21 +8,42 @@ const Trending = ({ data }) => {
   return (
     <div className={styles.app_top}>
       <div className={styles.title}>Top Categories</div>
+
       <div className={styles.list}>
         {data.map((event) => (
-          <Link key={event._id} href={`/categories/${event.category}`}>
+          <div key={event._id} class={styles.card}>
             <Image
+              class={styles.card_img}
               width={200}
               height={200}
               alt={event.title}
               src={event.image}
             />
-
-            <div>
-              <h2>{event.title}</h2>
-              <p>{event.description}</p>
+            <div class={styles.card_info}>
+              <p class={styles.text_body}>{event.description}</p>
+              <Link
+                class={styles.text_title}
+                key={event._id}
+                href={`/categories/${event.category}`}
+              >
+                {event.title}
+              </Link>
             </div>
-          </Link>
+          </div>
+
+          // <Link key={event._id} href={`/categories/${event.category}`}>
+          //   <Image
+          //     width={200}
+          //     height={200}
+          //     alt={event.title}
+          //     src={event.image}
+          //   />
+
+          //   <div>
+          //     <h2>{event.title}</h2>
+          //     <p>{event.description}</p>
+          //   </div>
+          // </Link>
         ))}
       </div>
     </div>
