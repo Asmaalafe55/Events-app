e0731f1443b738d7dc8fef664ee068c6b02c297a
Asmaalafe55 <asmaalafe55@gmail.com>
2023-02-16 23:54:27 +0200

    hegit of sign up

--- src/components/sign/RegisterPage.jsx
+++ src/components/sign/RegisterPage.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 import styles from './RegisterPage.module.scss';
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div>
+      <div>RegisterPage</div>
+      <div>RegisterPage</div>
+      <div>RegisterPage</div>
+      <div>RegisterPage</div>
+      <div>RegisterPage</div>
+      <div>RegisterPage</div>
+      <div>RegisterPage</div>
+      <div>RegisterPage</div>
+      <div>RegisterPage</div>
+      <div>RegisterPage</div>
+      RegisterPage
+    </div>
+  );
 };
 
 export default RegisterPage;
