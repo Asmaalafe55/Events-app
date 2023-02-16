d2a24e12fc9c45bde67639cb2a9fbf7848eaf387
Asmaalafe55 <asmaalafe55@gmail.com>
2023-02-16 23:39:20 +0200

    editing the layout for sign routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import MainLayout from '../src/components/layout/MainLayout';
+import SecondaryLayout from '../src/components/layout/SecondaryLayout';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps, router }) {
   if (router.pathname === '/sign-in' || router.pathname === '/sign-up') {
-    return <Component {...pageProps} />;
+    return (
+      <>
+        <SecondaryLayout>
+          <Component {...pageProps} />
+        </SecondaryLayout>
+      </>
+    );
   }
   return (
     <>
