d37941597addfe646844eaf56756f716528f47aa
Asmaalafe55 <asmaalafe55@gmail.com>
2023-02-16 23:22:28 +0200

    adding if statment to the _app  page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import MainLayout from '../src/components/layout/MainLayout';
 import '../styles/globals.scss';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
+  if (router.pathname === '/sign-in' || router.pathname === '/sign-up') {
+    return <Component {...pageProps} />;
+  }
   return (
     <>
       <MainLayout>
