9606322482a89a0cd131e83a75e580b607f8f752
Asmaalafe55 <asmaalafe55@gmail.com>
2023-02-17 00:03:26 +0200

    updated also the sign up page

--- src/components/sign/RegisterPage.jsx
+++ src/components/sign/RegisterPage.jsx
@@ -2,21 +2,7 @@ import React from 'react';
 import styles from './RegisterPage.module.scss';
 
 const RegisterPage = () => {
-  return (
-    <div>
-      <div>RegisterPage</div>
-      <div>RegisterPage</div>
-      <div>RegisterPage</div>
-      <div>RegisterPage</div>
-      <div>RegisterPage</div>
-      <div>RegisterPage</div>
-      <div>RegisterPage</div>
-      <div>RegisterPage</div>
-      <div>RegisterPage</div>
-      <div>RegisterPage</div>
-      RegisterPage
-    </div>
-  );
+  return <div className={styles.sign_up_page}>sign up</div>;
 };
 
 export default RegisterPage;
