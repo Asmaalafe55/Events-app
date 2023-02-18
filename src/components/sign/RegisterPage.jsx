45652ca41fe06eb46403daefd8589f6688ad6ec6
Asmaalafe55 <asmaalafe55@gmail.com>
2023-02-18 23:40:22 +0200

    names of the files

--- src/components/sign/RegisterPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './RegisterPage.module.scss';
-
-const RegisterPage = () => {
-  return <div className={styles.sign_up_page}>sign up</div>;
-};
-
-export default RegisterPage;
---
+++ src/components/sign/SignUpPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './SignUpPage.module.scss';
+
+const SignUpPage = () => {
+  return <div className={styles.sign_up_page}>sign up</div>;
+};
+
+export default SignUpPage;
