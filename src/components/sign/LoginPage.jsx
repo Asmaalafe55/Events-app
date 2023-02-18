331b23be782a0220d075f62a5b314cd063f4a950
Asmaalafe55 <asmaalafe55@gmail.com>
2023-02-18 23:39:34 +0200

    changing the names of the files

--- src/components/sign/LoginPage.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-import styles from './LoginPage.module.scss';
-
-const LoginPage = () => {
-  return <div className={styles.sign_in_page}>login</div>;
-};
-
-export default LoginPage;
---
+++ src/components/sign/SignInPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './SignInPage.module.scss';
+
+const SignInPage = () => {
+  return <div className={styles.sign_in_page}>login</div>;
+};
+
+export default SignInPage;
