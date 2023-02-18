d9c63dd8e62b33496030afb9db1b8be1adc05ca8
Asmaalafe55 <asmaalafe55@gmail.com>
2023-02-18 23:41:38 +0200

    routes

--- pages/sign-in/index.js
+++ pages/sign-in/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import LoginPage from '../../src/components/sign/LoginPage';
+import SignInPage from '../../src/components/sign/SignInPage';
 
 const SignIn = () => {
-  return <LoginPage />;
+  return <SignInPage />;
 };
 
 export default SignIn;
