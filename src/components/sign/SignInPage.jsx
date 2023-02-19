9f2c4bde82e530275bbb79b289777a7d47146661
Asmaalafe55 <asmaalafe55@gmail.com>
2023-02-19 22:22:10 +0200

    editing the css of sign in

--- src/components/sign/SignInPage.jsx
+++ src/components/sign/SignInPage.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import styles from './SignInPage.module.scss';
 
 const SignInPage = () => {
-  return <div className={styles.sign_in_page}>sign in</div>;
+  return (
+    <div className={styles.sign_in_page}>
+      <div className={styles.left_sign_in}>left side</div>
+      <div className={styles.right_sign_in}>right side</div>
+    </div>
+  );
 };
 
 export default SignInPage;
