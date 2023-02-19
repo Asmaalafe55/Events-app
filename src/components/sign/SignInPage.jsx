ab806d96a961f9ce740691971f4f2d3ffbde343a
Asmaalafe55 <asmaalafe55@gmail.com>
2023-02-19 22:31:42 +0200

    change

--- src/components/sign/SignInPage.jsx
+++ src/components/sign/SignInPage.jsx
@@ -4,8 +4,8 @@ import styles from './SignInPage.module.scss';
 const SignInPage = () => {
   return (
     <div className={styles.sign_in_page}>
-      <div className={styles.left_sign_in}>left side</div>
-      <div className={styles.right_sign_in}>right side</div>
+      <div className={styles.left_sign_in}>sign in left side</div>
+      <div className={styles.right_sign_in}>sign in right side</div>
     </div>
   );
 };
