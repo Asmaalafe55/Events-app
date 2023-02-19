809cb3bcf53bd6551b5532e3e7e28137e498b017
Asmaalafe55 <asmaalafe55@gmail.com>
2023-02-19 22:31:21 +0200

    adding the same style to the sign up page

--- src/components/sign/SignUpPage.jsx
+++ src/components/sign/SignUpPage.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import styles from './SignUpPage.module.scss';
 
 const SignUpPage = () => {
-  return <div className={styles.sign_up_page}>sign up</div>;
+  return (
+    <div className={styles.sign_up_page}>
+      <div className={styles.left_sign_up}>sign up left side</div>
+      <div className={styles.right_sign_up}>sign up right side</div>
+    </div>
+  );
 };
 
 export default SignUpPage;
