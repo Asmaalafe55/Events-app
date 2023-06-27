74030f338d2dcc9b87fd285154c23fec60dde080
Asmaalafe55 <asmaalafe55@gmail.com>
2023-06-27 14:41:26 +0300

    adding profile skeleton using antd

--- pages/profile/Profile.jsx
+++ pages/profile/Profile.jsx
@@ -1,8 +1,41 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import React from 'react';
+import style from './Profile.module.scss';
+import { Layout, Menu, theme } from 'antd';
+import {
+  FileOutlined,
+  PieChartOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+
+const { Content, Sider } = Layout;
+
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+const items = [
+  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Tom', '3'),
+    getItem('Bill', '4'),
+    getItem('Alex', '5'),
+  ]),
+
+  getItem('Files', '9', <FileOutlined />),
+];
 
 const ProfilePage = () => {
   const router = useRouter();
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
   useEffect(() => {
     const isLoggedIn = localStorage.getItem('accessToken');
@@ -13,7 +46,50 @@ const ProfilePage = () => {
     }
   }, [router]);
 
-  return <div>Profile page</div>;
+  return (
+    <div className={style.profile_page}>
+      <Layout
+        style={{
+          minHeight: '100vh',
+        }}
+      >
+        <Sider
+          width={200}
+          style={{
+            background: colorBgContainer,
+          }}
+        >
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{
+              height: '100%',
+              borderRight: 0,
+            }}
+            items={items}
+          />
+        </Sider>
+
+        <Layout
+          style={{
+            padding: '24px',
+          }}
+        >
+          <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+              background: colorBgContainer,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </div>
+  );
 };
 
 export default ProfilePage;
