f9f657f43d443d2261d60e53d0d45f10b7cc294e
Asmaalafe55 <asmaalafe55@gmail.com>
2023-09-25 17:52:25 +0300

    menu edit

--- components/profile/profileMenu/ProfileMenu.jsx
+++ components/profile/profileMenu/ProfileMenu.jsx
@@ -9,27 +9,43 @@ import {
 const { Sider } = Layout;
 
 const ProfileMenu = () => {
+  const items = [
+    {
+      key: '1',
+      icon: <UserOutlined />,
+      label: 'Profile',
+    },
+    {
+      key: 'sub1',
+      icon: <PieChartOutlined />,
+      label: 'Events',
+      children: [
+        { key: '3', label: 'Liked' },
+        { key: '4', label: 'Registered' },
+        { key: '5', label: 'Expired' },
+        { key: '6', label: 'Canceled' },
+        { key: '7', label: 'Deleted' },
+      ],
+    },
+    {
+      key: '9',
+      icon: <SettingOutlined />,
+      label: 'Settings',
+    },
+  ];
+
   return (
     <Sider width={200}>
       <Menu
         mode="inline"
         defaultSelectedKeys={['1']}
         defaultOpenKeys={['sub1']}
-      >
-        <Menu.Item key="1" icon={<UserOutlined />}>
-          Profile
-        </Menu.Item>
-        <Menu.SubMenu key="sub1" icon={<PieChartOutlined />} title="Events">
-          <Menu.Item key="3">Liked</Menu.Item>
-          <Menu.Item key="4">Registered</Menu.Item>
-          <Menu.Item key="5">Expired</Menu.Item>
-          <Menu.Item key="6">Canceled</Menu.Item>
-          <Menu.Item key="7">Deleted</Menu.Item>
-        </Menu.SubMenu>
-        <Menu.Item key="9" icon={<SettingOutlined />}>
-          Settings
-        </Menu.Item>
-      </Menu>
+        style={{
+          height: '100%',
+          borderRight: 0,
+        }}
+        items={items}
+      />
     </Sider>
   );
 };
