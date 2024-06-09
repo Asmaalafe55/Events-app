463ccedf50635be5e3725c38120958a592aa84e3
Asmaa Lafe <asmaalafe55@gmail.com>
2024-06-09 16:34:19 +0300

    adding list design for liked events page

--- components/profile/events/liked/LikedEvents.jsx
+++ components/profile/events/liked/LikedEvents.jsx
@@ -1,21 +1,68 @@
 import React from 'react';
-import { Card, Input, Avatar } from 'antd';
 import style from './LikedEvents.module.scss';
-const { Meta } = Card;
+import { LikeOutlined, MessageOutlined, StarOutlined } from '@ant-design/icons';
+import { List, Space } from 'antd';
 
 const LikedEvents = ({}) => {
+  const data = Array.from({
+    length: 30,
+  }).map((_, i) => ({
+    href: 'https://ant.design',
+    title: `ant design part ${i}`,
+    description:
+      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  }));
+  
+  const IconText = ({ icon, text }) => (
+    <Space>
+      {React.createElement(icon)}
+      {text}
+    </Space>
+  );
+  
+
   return (
-    <Card
-      style={{ width: 300 }}
-      cover={
-        <img
-          alt="example"
-          src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+    <List
+    itemLayout="vertical"
+    size="large"
+    pagination={{
+      onChange: (page) => {
+        console.log(page);
+      },
+      pageSize: 5,
+    }}
+    dataSource={data}
+    footer={
+      <div>
+        <b>ant design</b> footer part
+      </div>
+    }
+    renderItem={(item) => (
+      <List.Item
+        key={item.title}
+        actions={[
+          <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
+          <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+          <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+        ]}
+        extra={
+          <img
+            width={272}
+            alt="logo"
+            src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+          />
+        }
+      >
+        <List.Item.Meta
+          title={<a href={item.href}>{item.title}</a>}
+          description={item.description}
         />
-      }
-    >
-      kjbjljnkjb
-    </Card>
+        {item.content}
+      </List.Item>
+    )}
+  />
   );
 };
 
